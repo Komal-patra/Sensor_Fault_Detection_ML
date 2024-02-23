261d26fcc68456270b3a42caff839710b2847447
Komal-patra <komalfsds 2022@gmail.com>
2024-02-23 18:30:20 +0000

    Data Ingestion Coded and executed

--- Sensor_fault_detection/utils.py
+++ Sensor_fault_detection/utils.py
@@ -2,6 +2,8 @@ from .config import mongo_client
 import pandas as pd
 import logging
 import json
+from .exception import SensorException
+import sys
 
 def dump_csv_file_to_mongodb_collection(file_path:str, database_name:str, collection_name:str)->None:
     try:
@@ -17,5 +19,15 @@ def dump_csv_file_to_mongodb_collection(file_path:str, database_name:str, collec
         mongo_client[database_name][collection_name].insert_many(json_records)
 
     except Exception as e:
-        raise e
+        raise SensorException(e,sys)
 
+def export_collection_as_dataframe(database_name:str, collection_name:str)->pd.DataFrame:
+    try:
+        # fetching the document from mongodb into pandas dataframe
+        df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
+        # as we dont need the default id column from the document
+        if '_id' in df.columns.to_list():
+            df = df.drop('_id', axis=1)
+        return df
+    except Exception as e:
+        raise SensorException(e,sys)
